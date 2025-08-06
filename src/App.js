c3c1e95c98b328a6646ef81eddd613a7056ee814
smilesandbraces <smilesandbracesmunshipuliya@gmail.com>
2025-08-06 11:48:19 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+import InternationalPatients from './pages/InternationalPatients';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/international" element={<InternationalPatients />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
