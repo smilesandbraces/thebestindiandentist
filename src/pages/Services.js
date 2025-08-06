f68e03328c71c42ff52aa3e71566e4a3cb3f2825
smilesandbraces <smilesandbracesmunshipuliya@gmail.com>
2025-08-06 11:51:07 +0530

    Add files via upload

---
+++ src/pages/Services.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Services() {
+  return (
+    <div className="container py-5">
+      <h2>Our Services</h2>
+      <ul>
+        <li>Orthodontics: Braces & Clear Aligners</li>
+        <li>Dental Implants</li>
+        <li>Root Canal Treatment</li>
+        <li>Teeth Whitening</li>
+        <li>General Dentistry</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Services;
